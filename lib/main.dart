b521deb13f046612cbdba662f8c575e2487e5004
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2020-09-12 11:19:55 +0100

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,63 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
-    ),
-  );
+    );
+  }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int fistDiceNumber = 1;
+  int secondDiceNumber = 1;
+
+  void rollDice() {
+    setState(() {
+      fistDiceNumber = Random.secure().nextInt(6) + 1;
+      secondDiceNumber = Random.secure().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: rollDice,
+              child: Image.asset('images/dice$fistDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: rollDice,
+              child: Image.asset('images/dice$secondDiceNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
